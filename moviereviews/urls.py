b9964997fd1ea1a4690c7ee65116eef5273fbd11
vantaflex <dasaduni2006@gmail.com>
2026-07-28 12:29:23 -0500

    Visualización de películas con estilo

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('about/', movieViews.about, name='about'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
