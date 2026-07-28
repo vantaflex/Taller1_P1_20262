c03da8be09c2aee9103b4587c92780457620ccd7
vantaflex <dasaduni2006@gmail.com>
2026-07-28 12:26:03 -0500

    búsqueda de películas

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+
+BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
