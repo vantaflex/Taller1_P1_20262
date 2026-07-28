08bb7a1f1bce2324938c480da83f19623fb12ba3
vantaflex <dasaduni2006@gmail.com>
2026-07-28 11:32:07 -0500

    cambios2

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,5 @@ def home(request):
     return render(request, 'home.html', {'name':'David Santiago Duarte'})
 
 def about(request):
-    return HttpResponse("<h1>Welcome to About Page</h1>")
+    #return HttpResponse("<h1>Welcome to About Page</h1>")
+    return render(request, 'about.html')
